1e4594ea54eff2f7ef421fd33f4840ce32fab889
hejiang@tju.edu.cn <hejiang@tju.edu.cn>
2018-01-14 16:07:26 +0800

    ****

--- tmp/sh-run.cmd
+++ tmp/sh-run.cmd
@@ -1,16 +1,22 @@
 @echo off
 
-del /F /S /Q D:\hengrui\hejiang2000.github.com\tmp\sh-csv
-del /F /S /Q D:\hengrui\hejiang2000.github.com\tmp\sh-xls
-del /F /S /Q D:\hengrui\hejiang2000.github.com\tmp\sh-buy
-del /F /S /Q D:\hengrui\hejiang2000.github.com\tmp\sh-sell
-mkdir D:\hengrui\hejiang2000.github.com\tmp\sh-csv
-mkdir D:\hengrui\hejiang2000.github.com\tmp\sh-xls
-mkdir D:\hengrui\hejiang2000.github.com\tmp\sh-buy
-mkdir D:\hengrui\hejiang2000.github.com\tmp\sh-sell
+rmdir /S /Q  D:\hengrui\hejiang2000.github.com\tmp\sh-csv
+mkdir        D:\hengrui\hejiang2000.github.com\tmp\sh-csv
+del /F /S /Q D:\hengrui\hejiang2000.github.com\tmp\sh-600000.csv
 
 for /R D:\zd_pazq\vipdoc\sh\lday %%i in (*.day) do (
-python convert.py %%i D:\hengrui\hejiang2000.github.com\tmp\sh-csv\%%~ni.csv
+    for /f "tokens=2 delims=h" %%j in ("%%~ni") do (
+        if "%%j" gtr "600000" if "%%j" lss "700000" (
+            python convert.py %%i D:\hengrui\hejiang2000.github.com\tmp\sh-csv\sh%%~nj.csv D:\hengrui\hejiang2000.github.com\tmp\sh-600000.csv %%j
+        )
+    )
 )
 
-rem start /B cmd /C "C:\Program Files\Microsoft Office\Office15\EXCEL.EXE" D:\hengrui\hejiang2000.github.com\tmp\sh-marker.xlsm
+REM rmdir /S /Q D:\hengrui\hejiang2000.github.com\tmp\sh-xls
+REM rmdir /S /Q D:\hengrui\hejiang2000.github.com\tmp\sh-buy
+REM rmdir /S /Q D:\hengrui\hejiang2000.github.com\tmp\sh-sell
+REM mkdir       D:\hengrui\hejiang2000.github.com\tmp\sh-xls
+REM mkdir       D:\hengrui\hejiang2000.github.com\tmp\sh-buy
+REM mkdir       D:\hengrui\hejiang2000.github.com\tmp\sh-sell
+
+REM start /B cmd /C "C:\Program Files\Microsoft Office\Office15\EXCEL.EXE" D:\hengrui\hejiang2000.github.com\tmp\sh-marker.xlsm
